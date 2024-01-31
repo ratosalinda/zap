c80906da5cfa074023f54a0472368a93497a1a17
GabrieelAssis <gabriel.assis@veredastecnologia.com.br>
2024-01-31 16:42:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
 const { Client } = require('whatsapp-web.js');
 const client = new Client();
 
 client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
+  console.log('QR RECEIVED', qr);
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+  console.log('Client is ready!');
 });
 
 client.initialize();
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
